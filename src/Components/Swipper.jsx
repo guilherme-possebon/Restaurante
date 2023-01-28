548847a08e15a7a56f1d898c607a4c4429949d43
Guilherme Possebon <gpossebon67@gmail.com>
2023-01-28 18:02:14 -0300

    Tentando por o site no ar

--- src/Components/Swipper.jsx
+++ src/Components/Swipper.jsx
@@ -1,11 +1,6 @@
 import "./Css/Swipper.css"
 
 
-import ex1 from '/exemplo1.jpg'
-import ex2 from '/exemplo2.jpg'
-import ex3 from '/exemplo3.jpg'
-import ex4 from '/exemplo4.jpg'
-
 export default function Swipper() {
 // const swiper = new Swiper('.swiper', {
 //     // Optional parameters
