be78f9d636d3cd35b431a66d6ed241be1c6863fe
Guilherme Possebon <gpossebon67@gmail.com>
2023-02-02 14:51:41 -0300

    Tentando resolver um bug

--- src/App.jsx
+++ src/App.jsx
@@ -20,7 +20,7 @@ function App() {
       <Router>
             <Routes>
                 <Route exact path='/Restaurante/' element={<MainContent />}></Route>
-                <Route path='/Restaurante/Cardapio/' element={<Entrada />}></Route>
+                <Route exact path='/Restaurante/Cardapio/' element={<Entrada />}></Route>
                 <Route path='/Restaurante/Cardapio/pratos' element={<Pratos />}></Route>
                 <Route path='/Restaurante/Cardapio/porcao' element={<Porcao />}></Route>
                 <Route path='/Restaurante/Cardapio/bebidas' element={<Bebidas />}></Route>
