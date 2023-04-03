3e4f9474ea405051f7f413e33c96833397c4733c
Guilherme Possebon <gpossebon67@gmail.com>
2023-04-03 17:00:13 -0300

    teste de deploy com o vercel

--- src/pages/Components/LayoutLogin/_userLoginButton.tsx
+++ src/pages/Components/LayoutLogin/_userLoginButton.tsx
@@ -85,7 +85,7 @@ export default function UserLoginButton() {
                     }}
                     className={styles.Button}
                   >
-                    Verificar E-mail
+                    Verificar e-mail
                   </button>
                 </>
               )}
